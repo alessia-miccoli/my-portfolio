d5360fc14d80afdf16cd74b3e2c7613f4f3eb520
Alessia <miccoli.alessia@hotmail.it>
2019-08-28 17:25:51 +0200

    Updates

---
+++ precache-manifest.81a9bfe470dd697050a323587143569a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8bef003822c2ca636f34fbbc0bb1c84",
+    "url": "/alessia-miccoli/my-portfolio/public/index.html/index.html"
+  },
+  {
+    "revision": "20a435e36fe4beda9a11",
+    "url": "/alessia-miccoli/my-portfolio/public/index.html/static/css/main.f75be26c.chunk.css"
+  },
+  {
+    "revision": "ecc9664416954e76080e",
+    "url": "/alessia-miccoli/my-portfolio/public/index.html/static/js/2.58517254.chunk.js"
+  },
+  {
+    "revision": "20a435e36fe4beda9a11",
+    "url": "/alessia-miccoli/my-portfolio/public/index.html/static/js/main.f2f9d0f8.chunk.js"
+  },
+  {
+    "revision": "6f58d02f3a6d25e014c8",
+    "url": "/alessia-miccoli/my-portfolio/public/index.html/static/js/runtime~main.52902879.js"
+  }
+]);
