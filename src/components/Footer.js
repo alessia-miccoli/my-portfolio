ab5cc6597b64825b0711b76af70ba7c6f1b6e583
Alessia <miccoli.alessia@hotmail.it>
2020-02-23 11:53:10 +0100

    adjusted nav links and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component{
 
     render(){
         return (
-        <div className="card-footer text-muted text-center">
+        <div className="card-footer text-muted text-center credits">
             <p id="footer">Made with <i className="fab fa-bootstrap"></i>ootstrap and <i className="fab fa-react"></i>React</p>
         </div>);
     }
